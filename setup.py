28664c86e8ec14bd41df5add09dfd0a03aa02ef0
Ilja Orlovs <ilja@tells.so>
2013-03-16 11:51:47 +0000

    accidently release v0.2 to the pypy, need to override this mistake.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def find_packages(path, base="" ):
     return packages
 
 setup(name='vbox',
-    version='0.1.2',
+    version='0.2.1',
     description="VirtualBox CLI bindings",
     long_description=open("README.md").read(),
 
